2d60b9d82ff9eb5cbcb5922995087b18e8498e3f
dandnirv7 <dandnirv7@gmail.com>
2023-09-16 19:35:50 +0700

    initial setup

---
+++ app.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { initFlowbite } from "flowbite";
+onMounted(() => {
+  initFlowbite();
+});
+</script>
+<template>
+  <div>
+    <Head>
+      <Title>Shopcart | Shop with Ease</Title>
+    </Head>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
